c885b7f1de8167264bbda27cf58d7ffcf31674bf
Chris Vargas <gqcodes@gmail.com>
2023-02-08 09:37:39 -0500

    next 13

---
+++ next13-experiment/app/[movie]/page.jsx
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+
+// serve static pages on build instead of having to fetch them everytime on render
+const generateStaticParams = async () => {
+  const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`);
+  const res = await data.json();
+  return res.results.map((movie) => ({
+    movie: toString(movie.id),
+  }));
+};
+
+const MovieDetail = async ({ params }) => {
+  const { movie } = params;
+  const imagePath = 'https://image.tmdb.org/t/p/original';
+  const data = await fetch(`https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}`, { next: { revalidate: 60 } });
+  const res = await data.json();
+
+  return (
+    <div>
+      <div>
+        <h2 className='text-2xl'>{res.title}</h2>
+        <h2 className='text-lg'>{res.release_date}</h2>
+        <h2>Runtime: {res.runtime} minutes</h2>
+        <h2 className='bg-green-600 inline-block my-2 py-2 px-4 rounded-md text-sm'>{res.status}</h2>
+        <Image className='my-12 w-full' src={imagePath + res.backdrop_path} width={1000} height={1000} priority />
+        <p>{res.overview}</p>
+      </div>
+    </div>
+  );
+};
+
+export default MovieDetail;
